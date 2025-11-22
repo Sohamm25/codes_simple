cd00edfa1735a94811787cab489459da31a1c0c8
Soham Pawar <154597186+Sohamm25@users.noreply.github.com>
2025-11-22 19:59:39 +0530

    Fix indentation and add max function output

--- factorial.py
+++ factorial.py
@@ -4,8 +4,9 @@ user_input = input("Enter elements of the list separated by spaces: ")
 user_list = user_input.split()
 
 # Convert elements to appropriate data type if needed
-# For example, if you want integers:
+# For example, if you want integers: 
 user_list = [int(x) for x in user_list]
 print(user_list)
 print(min(user_list))
+
 print(max(user_list))
