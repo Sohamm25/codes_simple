0745179371d20768560e836b0e39fca782e4bfab
Soham Pawar <154597186+Sohamm25@users.noreply.github.com>
2026-03-08 23:48:08 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,12 +1,9 @@
 user_input = input("Enter elements of the list separated by spaces: ")
-
-# Split the input string into individual elements
 user_list = user_input.split()
 
-# Convert elements to appropriate data type if needed
-# For example, if you want integers: 
 user_list = [int(x) for x in user_list]
 print(user_list)
 print(min(user_list))
 
 print(max(user_list))
+
